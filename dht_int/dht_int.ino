01c7aac7adab1e889bbf2c39604e5352cd3b8657
inwooshin <dkrlehowl45@naver.com>
2022-01-10 18:19:29 +0900

    코드 보기 쉽게 변경

--- dht_int/dht_int.ino
+++ dht_int/dht_int.ino
@@ -6,52 +6,52 @@ Ticker tickerLed;
 
 void ICACHE_RAM_ATTR ledISR();
 
-int i =0, count = 0, cnt = 0, readHumid = 0, readTemp = 0, dt[82] = {0,}, before = micros();
+int callAllInt =0, count = 0, dt_Index = 0, readHumid = 0, readTemp = 0, dt[82] = {0,}, before = micros();
 
 void ICACHE_RAM_ATTR change(){
   //Serial.println(count);
-  if(i > 20 && i < 25){
-    i++;
-    dt[cnt * 2] = micros();
+  if(callAllInt > 20 && callAllInt < 25){
+    callAllInt++;
+    dt[dt_Index * 2] = micros();
   }
-  else if(i == 25){
-     dt[cnt * 2] = micros() - dt[cnt * 2];
-     dt[cnt * 2 + 1] = micros();
-     i = 26;
+  else if(callAllInt == 25){
+     dt[dt_Index * 2] = micros() - dt[dt_Index * 2];
+     dt[dt_Index * 2 + 1] = micros();
+     callAllInt = 26;
   }
-  else if(i == 26){
-     dt[cnt * 2 + 1] = micros() - dt[cnt * 2 + 1];  
-     i = 25;
+  else if(callAllInt == 26){
+     dt[dt_Index * 2 + 1] = micros() - dt[dt_Index * 2 + 1];  
+     callAllInt = 25;
      count++;
-     cnt++;
-     dt[cnt * 2] = micros();
+     dt_Index++;
+     dt[dt_Index * 2] = micros();
      if(count == 40){
-        for(cnt = 0 ; cnt < 8 ; cnt++){
+        for(dt_Index = 0 ; dt_Index < 8 ; dt_Index++){
           readHumid = readHumid << 1;
-          if(dt[cnt * 2 + 1] > 49){
+          if(dt[dht_Index * 2 + 1] > 49){
             readHumid = readHumid + 1;  
           } 
           else readHumid = readHumid + 0;
         }
     
-        for(cnt = 16 ; cnt < 24 ; cnt++){
+        for(dt_Index = 16 ; dt_Index < 24 ; dt_Index++){
           readTemp = readTemp << 1;
-          if(dt[cnt * 2 + 1] > 49){
+          if(dt[dt_Index * 2 + 1] > 49){
             readTemp = readTemp + 1;  
           } 
           else readTemp = readTemp + 0;
         }
+
         char buf[80];
         sprintf(buf, " Temp:%d, Humid:%d\r\n", readTemp, readHumid);
         Serial.println(buf);
 
         noInterrupts();
         tickerLed.attach_ms(6000,ledISR);
-
         
-        i = 0;
+        callAllInt = 0;
         count = 0;
-        cnt = 0; 
+        dt_Index = 0; 
         readTemp = 0;
         readHumid = 0;
      }
@@ -59,21 +59,21 @@ void ICACHE_RAM_ATTR change(){
 }
 
 void ICACHE_RAM_ATTR ledISR(){
-  if(i == 0){
-    i++;
+  if(callAllInt == 0){
+    callAllInt++;
     digitalWrite(DHT11PIN, 1);
     pinMode(DHT11PIN, OUTPUT);
     tickerLed.attach_ms(1,ledISR);
-    attachInterrupt(digitalPinToInterrupt(DHT11PIN), change, CHANGE);
   }
-  else if(i == 1){
-    i++;
+  else if(callAllInt == 1){
+    callAllInt++;
     digitalWrite(DHT11PIN, 0);
   }
-  else if(i > 1 && i < 20) i++;
-  else if(i == 20){
-    i++;
+  else if(callAllInt > 1 && callAllInt < 20) callAllInt++;
+  else if(callAllInt == 20){
+    callAllInt++;
     noInterrupts();
+    attachInterrupt(digitalPinToInterrupt(DHT11PIN), change, CHANGE);
     pinMode(DHT11PIN, INPUT_PULLUP);
   }
 }
